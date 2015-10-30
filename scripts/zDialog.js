e2186aa50fb066ee4847f801b6e4ae6b61146a1c
pwstrick <542491396@qq.com>
2015-10-30 14:06:25 +0800

    0.1.3 fixed bug
    0.1.3 fixed bug

--- scripts/zDialog.js
+++ scripts/zDialog.js
@@ -6,19 +6,11 @@
  */
 ;(function($, global){
 	'use strict';
-	
 	var _zDialog = null;
-	/**
-	 * $('xxx').zDialog()格式
-	 */
-	$.fn.zDialog = function(options) {
-        return new zDialog(this, options);
-    };
 	var zDialog = function (options) {
         this.options = $.extend({}, zDialog.defaults, options);//合并参数设置
         this.init();//进行一些初始化工作
     };
-    
     /**
      * 单例获取zDialog
      */
@@ -35,37 +27,42 @@
     	_zDialog = new zDialog(options);
     	return _zDialog;
     }
-        
     /**
      * 默认配置项
      */
     zDialog.defaults = {
+    	//容器
     	container: '.zdialog',//弹出框外层选择器 包括阴影和内容
-    	contentContainer: '.zdialog-content',//内容部分的选择器
+    	contentContainer: '.zdialog-content',//主题部分的选择器
     	messageContainer: '.zdialog-message',//消息信息的选择器
     	buttonContainer: '.zdialog-content',//按钮列表的选择器
     	layerContainer: '.zdialog-layer',//阴影的选择器
     	
+    	//内容
         content: "",//弹出框内容
-        eventType: "click",//按钮事件类型 默认是click，可以设置为tap
         
+        //外观
         left: "5%",
         right: "5%",//用left和right替代width宽度 因为手机屏幕不一样
         top: null,
+        fixed:false, //是否使用属性position:fixed
         
-        ok: function() {},//点击确定触发事件 false就不显示
+        //按钮
+        ok: false,//点击确定触发事件 false就不显示
         okValue: "确定",//确定文字
         okCSS: "btn btn-primary",
-        cancel: function() {},//点击取消触发事件 false就不显示
+        cancel: false,//点击取消触发事件 false就不显示
         cancelValue: "取消",//取消内容
         cancelCSS: "btn",
         buttons: [],//自定义按钮组 有三个属性[{value,callback,css}]
         
-        fixed:false, //是否使用属性position:fixed
+        //交互
+        eventType: "click",//按钮事件类型 默认是click，可以设置为tap
         quickClose:true, //点击空白处关闭
         opacity:0.6, //阴影层透明度 false表示完全透明
-        debug:true, //调试内容
- 
+        debug:false, //调试内容
+        
+        //事件
         onopen:function(){},//打开弹出框的时候触发
         onclose:function(){}//关闭事件的时候触发
     };
@@ -87,17 +84,14 @@
         	//销毁存在的
         	me.$container = $(this.options.container);
         	me.$container.remove();
-        	
         	//重新添加
             $('body').append(this.options.template);
-            
             //对象
             me.$container = $(this.options.container);
             me.$message = $(this.options.messageContainer);
             me.$button = $(this.options.buttonContainer);
             me.$content = $(this.options.contentContainer);
             me.$layer = $(this.options.layerContainer);
-            
             //左右位移 代替宽度
             if(me.options.left) {
             	me.$content.css('left', me.options.left);
@@ -105,11 +99,9 @@
             if(this.options.right) {
             	me.$content.css('right', me.options.right);
             }
-            
             me.layer();
             me.btn();
             me.position();
-            
             //快速关闭事件 就是点击背景层 隐藏弹出框
             if(me.options.quickClose === true) {
             	me.$layer.on(me.options.eventType, function() {
@@ -122,10 +114,10 @@
         	var fixed = this.options.fixed;
         	if(top == null) {//未设置值
         		if(fixed === true) {
-        			top = '40%';//直接写死 下面的计算会出问题
+        			top = '30%';//直接写死 下面的计算会出问题
         		}else {
         			var $window = $(window);
-			        var dt = $('body').scrollTop();//zepto需要这么做
+			        var dt = window.pageYOffset|| document.documentElement.scrollTop || document.body.scrollTop;
 			        var wh = $window.height();
 			        var oh = this.$content.height();
 			        var top = (wh - oh) * 382 / 1200 + dt;// 黄金比例
@@ -136,8 +128,6 @@
         	if(fixed === true) {//fixed定位
         		this.$content.css('position', 'fixed');
         	}
-        	
-			
         },
         layer: function() {//阴影层操作
         	var screen = window.screen.height;//屏幕高度
@@ -152,41 +142,30 @@
         open: function() {//显示弹出框
         	this.$message.html(this.options.content);
             this.$container.show();
-            //绑定弹出框的时候附加的事件
-            this.options.onopen.apply(this);
+            this.options.onopen.apply(this);//绑定弹出框的时候附加的事件
         },
         close: function() {//关闭弹出框
             this.$container.hide();
-            //绑定关闭框的时候附加的事件
-            this.options.onclose.apply(this);
+            this.options.onclose.apply(this);//绑定关闭框的时候附加的事件
         },
         btn: function () {//按钮事件绑定
-        	var buttons = this.options.buttons;
-        	var eventType = this.options.eventType;
-        	var ok = this.options.ok;
-        	var cancel = this.options.cancel;
+        	var me = this;
+        	var buttons = me.options.buttons;
+        	var ok = me.options.ok;
+        	var cancel = me.options.cancel;
         	var length = buttons.length;
+        	//先绑定自定义按钮
         	if(length > 0) {
         		//绑定自定义按钮
         		for(var i=0; i<length; i++) {
-        			var $custom = $('<button>').attr({'type':'button', 'class':buttons[i].css}).text(buttons[i].value)
-        			$custom.on(eventType, buttons[i].callback);
+        			var $custom = zDialog.btnBind(buttons[i].callback, buttons[i].css, buttons[i].value, me);
+        			me.$button.append($custom);
         		}
         	}
-        	
-        	var $ok='';
-        	if(ok !== false) {
-        		$ok = $('<button>').attr({'type':'button', 'class':this.options.okCSS}).text(this.options.okValue)
-        		$ok.on(eventType, ok);//绑定事件
-        	}
-        	this.$button.append($ok);
-        	
-        	var $cancel='';
-        	if(cancel !== false) {
-        		$cancel = $('<button>').attr({'type':'button', 'class':this.options.cancelCSS}).text(this.options.cancelValue)
-        		$cancel.on(eventType, cancel);//绑定事件
-        	}
-        	this.$button.append($cancel);
+        	var $ok = zDialog.btnBind(ok, me.options.okCSS, me.options.okValue, me);
+        	me.$button.append($ok);
+        	var $cancel = zDialog.btnBind(cancel, me.options.cancelCSS, me.options.cancelValue, me);
+        	me.$button.append($cancel);
         },
         console: function(msg) {//控制台调试
 	    	if(this.options.debug === false)
@@ -197,26 +176,47 @@
 	    }
     };
 
+	/**
+	 * 按钮生成
+	 * 绑定了事件后，默认都会关闭，只有当设置的函数返回false的时候，阻止关闭
+	 */
+	zDialog.btnBind = function(btn, css, value, me) {
+		var $btn='';
+		var eventType = me.options.eventType;
+		if(btn !== false) {
+        	$btn = $('<button>').attr({'type':'button', 'class':css}).text(value)
+        	if(btn === true) {
+        		$btn.on(eventType, function() {
+        			me.close();//默认关闭
+        		});
+        	}else if(typeof btn === 'function') {//绑定事件
+        		$btn.on(eventType, function() {
+        			var result = btn.apply(this);//如果返回false，则阻止关闭
+        			if(result !== false) {
+        				me.close();
+        			}
+        		});
+        	}
+        }
+		return $btn;
+	};
     /**
      * 判断两个值是否相同
      */
     zDialog.isEqual = function(a, b, strong) {
     	var aProps = Object.getOwnPropertyNames(a);
     	var bProps = Object.getOwnPropertyNames(b);
-    	if (aProps.length != bProps.length) {
+    	if (aProps.length != bProps.length)
     		return false;
-    	}
     	strong = strong || false;//强比较和弱比较
     	for (var i = 0; i < aProps.length; i++) {
     		var propName = aProps[i];
     		if(strong) {
-    			if (a[propName] !== b[propName]) {
+    			if (a[propName] !== b[propName])
 	    			return false;
-	    		}
     		}else {
-    			if (a[propName] != b[propName]) {
+    			if (a[propName] != b[propName])
 	    			return false;
-	    		}
     		}
     	}
     	return true;
